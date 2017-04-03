3fff389b04e5d87e21c263048c655d835864436e
samgrimm <samantha.grimm@gmail.com>
2017-04-03 10:04:15 -0500

    Initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
+  # GET /posts/1
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts
+  def create
+      @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to post_path(@post, locale:I18n.locale), notice: t(".post_succes")
+    else
+      render :new
+    end
+  end
+  # PATCH/PUT /posts/1
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post, locale:I18n.locale), notice: t(".post_updated")
+    else
+      render :edit
+    end
+  end
+  # DELETE /posts/1
+  def destroy
+    @post.destroy
+    redirect_to posts_url(locale: I18n.locale), notice: t(".post_destroyed")
+  end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+end
